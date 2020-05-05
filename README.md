f58856d5c7b52898b5bddbb04eded675b7012caa
Matthew Wrather <matt@wrathercreative.com>
2020-05-05 11:55:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Learn Functional Programming in Javascript
+
+This repository gives code samples illustrating how to refactor a small program using functional principles and the [Ramda](https://ramdajs.com/) JavaScript library.
+
+## System Requirements
+
+- [`git`](https://git-scm.com/)
+- [`NodeJS`](https://nodejs.org/)
+- [`npm`](https://npmjs.com/)
+
+## Setup
+
+Clone this repo and install dependencies.
+
+```shell
+git clone https://github.com/mwrather/poker-classifier-fp.git
+cd poker-classifier-fp
+npm install
+```
+
+Checkout
+
+```shell
+git checkout start         # the bare repo with no tests
+git checkout conventional  # pass tests with a conventional approach
+git checkout fp            # refactor using ramda
+```
+
+## Working
+
+As you experiment with different implementations, use the tests to check your work:
+
+```shell
+npm run test        # test the implementation once
+npm run test:watch  # re-run tests on file save
+```
---
+++ src/index.spec.js
@@ -0,0 +1,111 @@
+import classifyHand from '.';
+
+test('recognizes a royal flush', () => {
+  const result = classifyHand([
+    { rank: 10, suit: 'diamonds' },
+    { rank: 13, suit: 'diamonds' },
+    { rank: 1, suit: 'diamonds' },
+    { rank: 12, suit: 'diamonds' },
+    { rank: 11, suit: 'diamonds' },
+  ]);
+  expect(result).toBe('royalFlush');
+});
+
+test('recognizes a straight flush', () => {
+  const result = classifyHand([
+    { rank: 2, suit: 'clubs' },
+    { rank: 6, suit: 'clubs' },
+    { rank: 3, suit: 'clubs' },
+    { rank: 5, suit: 'clubs' },
+    { rank: 4, suit: 'clubs' },
+  ]);
+  expect(result).toBe('straightFlush');
+});
+
+test('recognizes four of a kind', () => {
+  const result = classifyHand([
+    { rank: 9, suit: 'clubs' },
+    { rank: 9, suit: 'diamonds' },
+    { rank: 9, suit: 'spades' },
+    { rank: 7, suit: 'hearts' },
+    { rank: 9, suit: 'hearts' },
+  ]);
+  expect(result).toBe('fourOfAKind');
+});
+
+test('recognizes a full house', () => {
+  const result = classifyHand([
+    { rank: 12, suit: 'clubs' },
+    { rank: 12, suit: 'diamonds' },
+    { rank: 12, suit: 'spades' },
+    { rank: 2, suit: 'hearts' },
+    { rank: 2, suit: 'clubs' },
+  ]);
+  expect(result).toBe('fullHouse');
+});
+
+test('recognizes a flush', () => {
+  const result = classifyHand([
+    { rank: 1, suit: 'clubs' },
+    { rank: 3, suit: 'clubs' },
+    { rank: 5, suit: 'clubs' },
+    { rank: 7, suit: 'clubs' },
+    { rank: 9, suit: 'clubs' },
+  ]);
+  expect(result).toBe('flush');
+});
+
+test('recognizes a straight', () => {
+  const result = classifyHand([
+    { rank: 4, suit: 'clubs' },
+    { rank: 6, suit: 'hearts' },
+    { rank: 7, suit: 'clubs' },
+    { rank: 5, suit: 'diamonds' },
+    { rank: 8, suit: 'clubs' },
+  ]);
+  expect(result).toBe('straight');
+});
+
+test('recognizes three of a kind', () => {
+  const result = classifyHand([
+    { rank: 4, suit: 'clubs' },
+    { rank: 4, suit: 'diamonds' },
+    { rank: 4, suit: 'spades' },
+    { rank: 11, suit: 'hearts' },
+    { rank: 2, suit: 'clubs' },
+  ]);
+  expect(result).toBe('threeOfAKind');
+});
+
+test('recognizes two pair', () => {
+  const result = classifyHand([
+    { rank: 11, suit: 'clubs' },
+    { rank: 9, suit: 'diamonds' },
+    { rank: 5, suit: 'spades' },
+    { rank: 11, suit: 'hearts' },
+    { rank: 9, suit: 'clubs' },
+  ]);
+  expect(result).toBe('twoPair');
+});
+
+test('recgnizes one pair', () => {
+  const result = classifyHand([
+    { rank: 1, suit: 'clubs' },
+    { rank: 3, suit: 'diamonds' },
+    { rank: 9, suit: 'spades' },
+    { rank: 7, suit: 'hearts' },
+    { rank: 9, suit: 'clubs' },
+  ]);
+  expect(result).toBe('pair');
+});
+
+test('returns none otherwise', () => {
+  const result = classifyHand([
+    { rank: 1, suit: 'clubs' },
+    { rank: 3, suit: 'diamonds' },
+    { rank: 5, suit: 'spades' },
+    { rank: 7, suit: 'hearts' },
+    { rank: 9, suit: 'clubs' },
+  ]);
+  expect(result).toBe('none');
+});
